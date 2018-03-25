6ba7b96990af39fbae6610c5c1e6f6cd51aafddf
Sulley01 <sullivan.alvin@ymail.com>
2018-03-26 01:19:03 +0700

    Implemented shaded algorithm

--- shaded-car/main.cpp
+++ shaded-car/main.cpp
@@ -9,7 +9,13 @@
 #include <GLFW/glfw3.h>
 GLFWwindow* window;
 
-#include <shader.hpp>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include <common/shader.hpp>
+
+#include <math.h>
 
 int main(void)
 {
@@ -38,7 +44,6 @@ int main(void)
 	glfwMakeContextCurrent(window);
 
 	// Initialize GLEW
-	glewExperimental = true; // Needed for core profile
 	if (glewInit() != GLEW_OK) {
 		fprintf(stderr, "Failed to initialize GLEW\n");
 		getchar();
@@ -52,6 +57,55 @@ int main(void)
 	// Background
 	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
 
+	GLfloat g_vertex_buffer_data[] = {
+		-0.9f, -0.4f, 0.0f,
+		-0.9f, 0.2f, 0.0f,
+		-0.8f, 0.6f, 0.0f,
+		0.2f, 0.6f, 0.0f,
+		0.9f, 0.1f, 0.0f,
+		0.9f, -0.4f, 0.0f,
+	};
+
+	GLfloat g_vertex2_buffer_data[] = {
+		-0.7f, -0.45f, 0.0f,
+		-0.7f, -0.35f, 0.0f,
+		-0.6f, -0.25f, 0.0f,
+		-0.5f, -0.35f, 0.0f,
+		-0.5f, -0.45f, 0.0f,
+		-0.6f, -0.55f, 0.0f,
+	};
+
+	GLfloat g_vertex3_buffer_data[] = {
+		0.7f, -0.45f, 0.0f,
+		0.7f, -0.35f, 0.0f,
+		0.6f, -0.25f, 0.0f,
+		0.5f, -0.35f, 0.0f,
+		0.5f, -0.45f, 0.0f,
+		0.6f, -0.55f, 0.0f,
+	};
+
+	GLuint g_element_buffer_data[] = {
+		0, 1, 2,
+		0, 2, 3,
+		0, 3, 4,
+		0, 4, 5,
+	};
+
+	GLuint g_element2_buffer_data[] = {
+		0, 1, 2,
+		0, 2, 3,
+		0, 3, 4,
+		0, 4, 5,
+	};
+
+	GLuint g_element3_buffer_data[] = {
+		0, 1, 2,
+		0, 2, 3,
+		0, 3, 4,
+		0, 4, 5,
+	};
+
+	// Object 1
 	// Create Vertex Array Object
 	GLuint VertexArrayID;
 	glGenVertexArrays(1, &VertexArrayID);
@@ -61,81 +115,211 @@ int main(void)
 	GLuint vertexbuffer;
 	glGenBuffers(1, &vertexbuffer);
 
-	static const GLfloat g_vertex_buffer_data[] = {
-		-0.9f, -0.4f, 0.0f, 0.1f, 0.0f, 0.0f,
-		-0.9f, 0.2f, 0.0f, 0.5f, 0.0f, 0.0f,
-		-0.8f, 0.6f, 0.0f, 1.0f, 0.0f, 0.0f,
-		0.2f, 0.6f, 0.0f, 1.0f, 0.0f, 0.0f,
-		0.9f, 0.1f, 0.0f, 0.5f, 0.0f, 0.0f,
-		0.9f, -0.4f, 0.0f, 0.1f, 0.0f, 0.0f
-	};
-
 	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+
+	// 1rst attribute buffer : vertices
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(
+		0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
+		3,                  // size
+		GL_FLOAT,           // type
+		GL_FALSE,           // normalized?
+		0,                  // stride
+		(void*)0            // array buffer offset
+	);
 
 	// Create an element array
 	GLuint elementbuffer;
 	glGenBuffers(1, &elementbuffer);
 
-	GLuint g_element_buffer_data[] = {
-		0, 1, 2,
-		0, 2, 3,
-		0, 3, 4,
-		0, 4, 5
-	};
-
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, elementbuffer);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(g_element_buffer_data), g_element_buffer_data, GL_STATIC_DRAW);
+
+	// Object 2
+	// Create Vertex Array Object
+	GLuint VertexArrayID2;
+	glGenVertexArrays(1, &VertexArrayID2);
+	glBindVertexArray(VertexArrayID2);
+
+	// Create a Vertex Buffer Object and copy the vertex data to it
+	GLuint vertexbuffer2;
+	glGenBuffers(1, &vertexbuffer2);
+
+	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer2);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex2_buffer_data), g_vertex2_buffer_data, GL_STATIC_DRAW);
+
+	// 2nd attribute buffer : vertices
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(
+		0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
+		3,                  // size
+		GL_FLOAT,           // type
+		GL_FALSE,           // normalized?
+		0,                  // stride
+		(void*)0            // array buffer offset
+	);
+
+	// Create an element array
+	GLuint elementbuffer2;
+	glGenBuffers(1, &elementbuffer2);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, elementbuffer2);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(g_element2_buffer_data), g_element2_buffer_data, GL_STATIC_DRAW);
+
+	// Object 3
+	// Create Vertex Array Object
+	GLuint VertexArrayID3;
+	glGenVertexArrays(1, &VertexArrayID3);
+	glBindVertexArray(VertexArrayID3);
+
+	// Create a Vertex Buffer Object and copy the vertex data to it
+	GLuint vertexbuffer3;
+	glGenBuffers(1, &vertexbuffer3);
+
+	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer3);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex3_buffer_data), g_vertex3_buffer_data, GL_STATIC_DRAW);
+
+	// 3rd attribute buffer : vertices
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(
+		0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
+		3,                  // size
+		GL_FLOAT,           // type
+		GL_FALSE,           // normalized?
+		0,                  // stride
+		(void*)0            // array buffer offset
+	);
+
+	// Create an element array
+	GLuint elementbuffer3;
+	glGenBuffers(1, &elementbuffer3);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, elementbuffer3);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(g_element3_buffer_data), g_element3_buffer_data, GL_STATIC_DRAW);
 
 	// Create and compile our GLSL program from the shaders
 	GLuint programID = LoadShaders("SimpleVertexShader.vertexshader", "SimpleFragmentShader.fragmentshader");
 
-	// 1rst attribute buffer : vertices
-	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(
-		0,								// attribute 0
-		3,								// size
-		GL_FLOAT,						// type
-		GL_FALSE,						// normalized?
-		sizeof(GL_FLOAT) * 6,			// stride
-		(void*)0						// array buffer offset
-	);
+	// Get a handle for our "MVP" uniform
+	GLuint MatrixID = glGetUniformLocation(programID, "MVP");
 
-	// 2nd attribute buffer : vertices color
-	glEnableVertexAttribArray(1);
-	glVertexAttribPointer(
-		1,								// attribute 1
-		3,								// size
-		GL_FLOAT,						// type
-		GL_FALSE,						// normalized?
-		sizeof(GL_FLOAT) * 6,			// stride
-		(void*)(sizeof(GL_FLOAT) * 3)	// array buffer offset
-	);
+	float angle = 0;
+
+	glm::mat4 translation = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, 0.0f));
+	glm::mat4 rotation = glm::rotate(glm::mat4(1.0f), glm::radians(0.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+	glm::mat4 translation2 = glm::translate(glm::mat4(1.0f), glm::vec3(-0.0f, -0.0f, 0.0f));
+	glm::mat4 MVP = translation2 * rotation * translation;
 
 	do {
-
-		// Clear the screen
+		// Clear the screen. It's not mentioned before Tutorial 02, but it can cause flickering, so it's there nonetheless.
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		// Use our shader
 		glUseProgram(programID);
 
+		// Draw object 1
+		glBindVertexArray(VertexArrayID);
+		glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+		glEnableVertexAttribArray(0);
+		glVertexAttribPointer(
+			0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
+			3,                  // size
+			GL_FLOAT,           // type
+			GL_FALSE,           // normalized?
+			0,                  // stride
+			(void*)0            // array buffer offset
+		);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, elementbuffer);
+
+		// Send our transformation to the currently bound shader, 
+		// in the "MVP" uniform
+		glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP[0][0]);
+		// Rotate object 2
+		translation = glm::translate(glm::mat4(1.0f), glm::vec3(0.6f, 0.4f, 0.0f));
+		rotation = glm::rotate(glm::mat4(1.0f), glm::radians(angle), glm::vec3(0.0f, 0.0f, 1.0f));
+		translation2 = glm::translate(glm::mat4(1.0f), glm::vec3(-0.6f, -0.4f, 0.0f));
+		MVP = translation2 * rotation * translation;
+
 		glDrawElements(GL_TRIANGLES, 12, GL_UNSIGNED_INT, 0);
+
+		// Draw object 2
+		glBindVertexArray(VertexArrayID2);
+		glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer2);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex2_buffer_data), g_vertex2_buffer_data, GL_STATIC_DRAW);
+		glEnableVertexAttribArray(0);
+		glVertexAttribPointer(
+			0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
+			3,                  // size
+			GL_FLOAT,           // type
+			GL_FALSE,           // normalized?
+			0,                  // stride
+			(void*)0            // array buffer offset
+		);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, elementbuffer2);
+
+		// Send our transformation to the currently bound shader, 
+		// in the "MVP" uniform
+		glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP[0][0]);
+		// Rotate object 3
+		translation = glm::translate(glm::mat4(1.0f), glm::vec3(-0.6f, 0.4f, 0.0f));
+		rotation = glm::rotate(glm::mat4(1.0f), glm::radians(angle), glm::vec3(0.0f, 0.0f, 1.0f));
+		translation2 = glm::translate(glm::mat4(1.0f), glm::vec3(0.6f, -0.4f, 0.0f));
+		MVP = translation2 * rotation * translation;
+
+		glDrawElements(GL_TRIANGLES, 12, GL_UNSIGNED_INT, 0);
+
+		// Draw object 3
+		glBindVertexArray(VertexArrayID3);
+		glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer3);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex3_buffer_data), g_vertex3_buffer_data, GL_STATIC_DRAW);
+		glEnableVertexAttribArray(0);
+		glVertexAttribPointer(
+			0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
+			3,                  // size
+			GL_FLOAT,           // type
+			GL_FALSE,           // normalized?
+			0,                  // stride
+			(void*)0            // array buffer offset
+		);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, elementbuffer3);
+
+		// Send our transformation to the currently bound shader, 
+		// in the "MVP" uniform
+		glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP[0][0]);
+		// Rotate object 1
+		translation = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, 0.0f));
+		rotation = glm::rotate(glm::mat4(1.0f), glm::radians(0.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+		translation2 = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, 0.0f));
+		MVP = translation2 * rotation * translation;
+
+		glDrawElements(GL_TRIANGLES, 12, GL_UNSIGNED_INT, 0);
+
+		glDisableVertexAttribArray(0);
+		glDisableVertexAttribArray(1);
+		glDisableVertexAttribArray(2);
 
 		// Swap buffers
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 
+		angle -= 1.0f;
+
 	} // Check if the ESC key was pressed or the window was closed
 	while (glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS &&
 		glfwWindowShouldClose(window) == 0);
 
-	glDisableVertexAttribArray(0);
-
 	// Cleanup VBO
 	glDeleteBuffers(1, &elementbuffer);
+	glDeleteBuffers(1, &elementbuffer2);
+	glDeleteBuffers(1, &elementbuffer3);
 	glDeleteBuffers(1, &vertexbuffer);
+	glDeleteBuffers(1, &vertexbuffer2);
+	glDeleteBuffers(1, &vertexbuffer3);
 	glDeleteVertexArrays(1, &VertexArrayID);
+	glDeleteVertexArrays(1, &VertexArrayID2);
+	glDeleteVertexArrays(1, &VertexArrayID3);
 	glDeleteProgram(programID);
 
 	// Close OpenGL window and terminate GLFW
